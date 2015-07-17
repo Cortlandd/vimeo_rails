4205644e7163837c5b651f2341de01748bb281c9
Cortland Walker <Cortland1568@gmail.com>
2015-07-17 15:17:43 -0400

    Remove access token

--- lib/vimeo/base.rb
+++ lib/vimeo/base.rb
@@ -7,5 +7,3 @@ module Vimeo
     base_uri 'https://api.vimeo.com' # 'should have: /?access_token=ACCESS_TOKEN at the end of all links'
   end
 end
-
-# request = https://api.vimeo.com/groups/1615/videos?access_token=23bbdffeb02bdf37287d5c8f866b8aa9
