71f2161e71c1bc72ab0a1abfe9af2d4fe9467283
Cortland Walker <cortland1568@gmail.com>
2016-06-14 01:53:50 -0500

    Require configuration before all other files in the gem.

--- lib/vimeo.rb
+++ lib/vimeo.rb
@@ -1,6 +1,6 @@
+require 'vimeo/configuration'
 require 'httparty'
 require 'net/http/post/multipart'
-require '../vimeo/*.rb'
 
 # The first file loaded by default when the gem is required by Bundler.
 
