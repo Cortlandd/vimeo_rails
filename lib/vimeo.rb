49e4663b24d666110b6f880bb27acac103b95acd
Cortland Walker <cortland1568@gmail.com>
2016-06-14 02:04:15 -0500

    Module Configuration updates.

--- lib/vimeo.rb
+++ lib/vimeo.rb
@@ -5,5 +5,25 @@ require 'net/http/post/multipart'
 # The first file loaded by default when the gem is required by Bundler.
 
 module Vimeo
-  # Your code goes here....
+
+  # Because we want users to be able to both read and
+  # write their configuration settings.
+  class << self
+  	attr_accessor :configuration
+  end
+
+  def self.configuration
+  	@configuration ||= Configuration.new
+  end
+
+  # Ehh, someone might need it.
+  def self.reset
+    @configuration = Configuration.new
+  end
+
+  # The part that says a block will be passed
+  def self.configure
+    yield(configuration)
+  end
+
 end
